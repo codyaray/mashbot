f2622b687b0a890d0489b96d71d7d1f202f86085
Cody A. Ray <sites@codyaray.com>
2009-11-15 15:13:09 -0500

    added sane routes (including login/logout/register and a home route mapped to the comatose micro-cms), and added the rails-footnotes plugin for easier access to debugging info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,7 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# displays footnotes in your application for easy debugging, such as 
+# sessions, request parameters, cookies, filter chain, routes, queries, etc.
+config.gem "rails-footnotes", :source => "http://gemcutter.org"
--- config/routes.rb
+++ config/routes.rb
@@ -31,10 +31,14 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+  #map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
 
-  map.resource :account, :controller => "users"
+  map.resource :account, :controller => :users
   map.resources :users
+
+  map.register 'register', :controller => 'users', :action => "new"
+  map.login 'login', :controller => 'user_sessions', :action => "new"
+  map.logout 'logout', :controller => 'user_sessions', :action => "destroy"
 
   map.resources :password_resets
 
