fcab203a67d954bdb72e17749ee03a616b9904cc
bluepojo <bluepojo@gmail.com>
2010-01-07 16:30:04 -0500

    Created js class for calendar events. Need to see if it'll actually work, though.

--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -3,15 +3,40 @@ function init_schedule(){
   $('#accordion').accordion();
 }
 
+/**
+ * @cfg id : integer or string
+ * @cfg title : string
+ * @cfg allDay : boolean
+ * @cfg date : Date
+ * @cfg start : Date
+ * @cfg end : Date
+ * @cfg url : string
+ * @cfg className : string
+ * @cfg editable : boolean
+ * @cfg source : reference to data structure event came from
+ */
 function CalEvent(config) {
-  this.id = undefined; // Integer or String - Unique unless repeating event
-  this.title = undefined; // String
-  this.allDay = true; // Boolean
-  this.date = undefined; //
+  this.id = undefined;
+  this.title = undefined;
+  this.allDay = true;
+  this.date = undefined;
   this.start = undefined;
-  this.
+  this.end = undefined;
+  this.url = undefined;
+  this.className = undefined;
+  this.editable = undefined;
+  this.source = undefined;
   this.init = function(config) {
-
+this.id = undefined;
+    this.title = config.title;
+    this.allDay = config.allDay;
+    this.date = config.date;
+    this.start = config.start;
+    this.end = config.end;
+    this.url = config.url;
+    this.className = config.className;
+    this.editable = config.editable;
+    this.source = config.source;
   };
 
   this.init(config);
