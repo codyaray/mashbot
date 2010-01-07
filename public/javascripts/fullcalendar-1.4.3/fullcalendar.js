5c12ea98640f1beffe4c86413d860e7bde523c9b
Andrew E. Gall <andrew@mashbot.net>
2010-01-07 13:21:04 -0800

    Added the all important month of YourMomuary

--- public/javascripts/fullcalendar-1.4.3/fullcalendar.js
+++ public/javascripts/fullcalendar-1.4.3/fullcalendar.js
@@ -66,7 +66,7 @@ var defaults = {
 	// locale
 	isRTL: false,
 	firstDay: 0,
-	monthNames: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+	monthNames: ['YourMomuary','February','March','April','May','June','July','August','September','October','November','December'],
 	monthNamesShort: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
 	dayNames: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
 	dayNamesShort: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
