c0bc97244f46d4dc298132436d90f5a88543cc23
bluepojo <bluepojo@gmail.com>
2010-01-07 15:52:45 -0500

    Added root route, temporarily. Eventually this will go to a landing page for login.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.resource :user_session
-  #map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+  map.root :controller => "dashboard"
 
   map.resource :account, :controller => :users
   map.resources :users
--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -1,4 +1,18 @@
-$(document).ready(function(){
-		    $('#schedule').fullCalendar({});
-		    $('#accordion').accordion();
-		  });
+function init_schedule(){
+  $('#schedule').fullCalendar({});
+  $('#accordion').accordion();
+}
+
+function CalEvent(config) {
+  this.id = undefined; // Integer or String - Unique unless repeating event
+  this.title = undefined; // String
+  this.allDay = true; // Boolean
+  this.date = undefined; //
+  this.start = undefined;
+  this.
+  this.init = function(config) {
+
+  };
+
+  this.init(config);
+}
