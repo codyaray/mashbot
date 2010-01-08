328b3889b3be1df34c3e3ea9463982064d24e5e9
bluepojo <bluepojo@gmail.com>
2010-01-08 01:03:01 -0500

    Dragging an item from the list on the left now adds an event to the calendar.

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,4 +1,6 @@
 class ScheduleController < ApplicationController
   def index
+    
   end
+
 end
--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -1,6 +1,36 @@
 function init_schedule(){
   $('#schedule').fullCalendar({});
   $('#accordion').accordion();
+  add_draggable_to_unscheduled_units();
+}
+
+function add_draggable_to_unscheduled_units(){
+  $('.schedule-unit').draggable({
+				  start: function(event, ui){
+				    var classes = ui.helper.attr('class').split(/\s/);
+				    var klass = undefined;
+				    var unit_id = undefined;
+				    for (var i = 0; i < classes.length; i++) {
+				      klass = classes[i];
+				      if (/unit\d+/.test(klass)){
+					unit_id = klass.replace('unit', '');
+					break;
+				      }
+				    }
+				    if (unit_id != undefined) {
+				      // TODO: Make spinner to indicate
+				      // event info is loading
+				      $.getJSON('/exampleEvent', { id: unit_id },
+					       function(json){
+						 $('#schedule').fullCalendar('renderEvent', json, true);
+					       });
+				    }
+				    // for (i in ui['helper']) {
+				    //   $('body').append(i + ': ' + ui['helper'][i] + '<br />');
+				    // }
+				  }
+				});
+
 }
 
 /**
@@ -27,7 +57,7 @@ function CalEvent(config) {
   this.editable = undefined;
   this.source = undefined;
   this.init = function(config) {
-this.id = undefined;
+    this.id = config.id;
     this.title = config.title;
     this.allDay = config.allDay;
     this.date = config.date;
