a45b0e7a2f9316e05eb800ad37c7fc61f5ad33e4
bluepojo <bluepojo@gmail.com>
2010-01-15 00:59:14 -0500

    1 gem at a time till heroku stops crying. +1

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   config.gem "capybara"
   config.gem "treetop"
   config.gem "polyglot"
+  config.gem "term-ansicolor"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
