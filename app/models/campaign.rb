5fcabad1f320ab1d6955ed6cbba6ccb02160d136
bluepojo <bluepojo@gmail.com>
2010-01-30 22:24:13 -0500

    Fixed broken scenario

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,13 +3,13 @@ class Campaign < ActiveRecord::Base
   validates_presence_of :title
 
   def must_end_after_start
-    if end_date != '' and start_date != '' and end_date < start_date
+    if !end_date.nil? and !start_date.nil? and end_date != '' and start_date != '' and end_date < start_date
       errors.add_to_base("Hold up! You can't end something before you start it!")
     end
   end
   
   def must_have_start_if_have_end
-    if end_date != '' and start_date == ''
+    if !end_date.nil? and !start_date.nil? and end_date != '' and start_date == ''
       errors.add_to_base("Whoa buddy! You're going to need a start date if you intend to have an end date")
     end
   end
