f94852106d5c62d5ac5801b78619ca1d80cb8137
bluepojo <bluepojo@gmail.com>
2010-04-22 14:57:30 -0400

    Twitter oauth works

--- app/controllers/authentication_credentials_controller.rb
+++ app/controllers/authentication_credentials_controller.rb
@@ -3,19 +3,48 @@ class AuthenticationCredentialsController < ApplicationController
   end
 
   def twitter
-    client = TwitterOAuth::Client.new(
-                                      :consumer_key => 'hj5zBtPPsgt232o0VVZeeA',
-                                      :consumer_secret => 'vASjnRHsClgVp7DsvRJaPBixXyirgJ2wcNEZUeTRF4'
-                                      )
+    key =  'hj5zBtPPsgt232o0VVZeeA'
+    secret = 'vASjnRHsClgVp7DsvRJaPBixXyirgJ2wcNEZUeTRF4'
+    client = TwitterOAuth::Client.new(:consumer_key => key, :consumer_secret => secret)
     request_token = client.request_token(:oauth_callback => 'http://mashbot.dev/authentication_credentials/twitter')
     @token_request_url = request_token.authorize_url
 
+    exists = false
+    @existing = AuthenticationCredential.all(:conditions => {:service => 'Twitter'})
     if params[:oauth_token] and params[:oauth_verifier]
       access_token = 
         client.authorize(params[:oauth_token],
                          request_token.secret,
                          :oauth_verifier => params[:oauth_verifier])
-      AuthenticationCredential.create({:service => 'Twitter', :details => {:token => access_token}})
+      
+      @existing.each do |a|
+        matching_record = nil
+        if a.details[:screen_name] == access_token.params[:screen_name]
+          exists = true
+          matching_record = a
+        end
+      end
+      
+      details = {
+        :screen_name => access_token.params[:screen_name],
+        :token => access_token.token,
+        :key => key,
+        :secret => access_token.secret
+      }
+      begin
+        
+        if not exists
+          AuthenticationCredential.create!({:service => 'Twitter', :details => details})
+        else
+          matching_record.details = details
+          matching_record.save!
+        end
+      rescue Exception => e
+        flash[:notice] = "Something went wrong saving the credentials for Twitter.  Try again? #{e.inspect}" 
+      else 
+        flash[:notice] = "Awesome, we have credentials for Twitter stored away"
+        @existing = AuthenticationCredential.all(:conditions => {:service => 'Twitter'}) # So we have the most recently stored one.
+      end
       
     end
 
