6e5985db3de0898ce8ddbf6cfd0a8d80eb6c02ee
bluepojo <bluepojo@gmail.com>
2010-05-11 16:58:34 -0400

    Fixed timepickr bug

--- public/javascripts/pickers.js
+++ public/javascripts/pickers.js
@@ -7,5 +7,8 @@ $(document).ready(function(){
 
 $(document).mousemove(function(){
                         var left = $('#go_live_time').position().left;
+                        var top = $('#go_live_time').position().top + 60;
                         $('.ui-timepickr').css('left', left);
+                        $('.ui-timepickr').css('top', top);
+                        $('.ui-timepickr').css('z-index', 1000);
                       });
