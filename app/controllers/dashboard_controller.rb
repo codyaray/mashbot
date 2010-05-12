dcd1ae045745fbc42821b0ae9226c64443fe7f98
Cody A. Ray <sites@codyaray.com>
2010-05-11 20:15:38 -0400

    change dashboard index to haml and do a quick hack to show services connected and available on the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class DashboardController < ApplicationController
 ##  before_filter :require_user ## Uncomment to session block this page
   def index
+    @entries = AuthenticationCredential.all(:conditions => {:service => 'Flickr'})
+    @service_accounts = ServiceAccount.all
+    @consumer_tokens=ConsumerToken.all :conditions=>{:user_id=>current_user.id}
+    @services=OAUTH_CREDENTIALS.keys-@consumer_tokens.collect{|c| c.class.service_name}
+    
   end
 
 end
