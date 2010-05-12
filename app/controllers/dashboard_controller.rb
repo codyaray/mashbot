8c3060bc25098206b61c7e4003bbf861a78ff9e9
Cody A. Ray <sites@codyaray.com>
2010-05-11 22:39:16 -0400

    trying to unify the authentication interface a bit more

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,7 @@ class DashboardController < ApplicationController
     @service_accounts = ServiceAccount.all
     @consumer_tokens=ConsumerToken.all :conditions=>{:user_id=>current_user.id}
     @services=OAUTH_CREDENTIALS.keys-@consumer_tokens.collect{|c| c.class.service_name}
-    
+    @token_request_url = FlickRaw.auth_url :perms => 'write'
   end
 
 end
